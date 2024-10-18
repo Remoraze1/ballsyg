c7568dcd581caaef6f01f06be1d920e6201a8f2b
nothingrem1 <160821424+Remoraze1@users.noreply.github.com>
2024-10-18 13:49:09 +1030

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-const images = ['egg.jpg', 'ussr.jpg', 'pumkin.jpeg']; // List your images here
 const texts = ['ballsyg', 'Discover Amazing Content', 'justice'];
 
-let currentImageIndex = 0;
 let currentTextIndex = 0;
 
-function changeImageAndText() {
-    currentImageIndex = (currentImageIndex + 1) % images.length;
+function changeText() {
     currentTextIndex = (currentTextIndex + 1) % texts.length;
-
-    document.getElementById('backgroundImage').src = images[currentImageIndex];
     document.getElementById('changingText').textContent = texts[currentTextIndex];
 }
 
-// Change image and text every 20 seconds
-setInterval(changeImageAndText, 20000);
+// Change text every 20 seconds
+setInterval(changeText, 20000);
+
 
 
